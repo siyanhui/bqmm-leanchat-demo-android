20a98a97b56e80592508a1d4d37d9aa6534f5611
wli <wli@leancloud.rocks>
2015-09-22 15:25:45 +0800

    添加新的 AVChatActivity

--- leanchat/src/main/java/com/avoscloud/chat/ui/chat/ChatRoomActivity.java
+++ leanchat/src/main/java/com/avoscloud/chat/ui/chat/ChatRoomActivity.java
@@ -1,103 +1,43 @@
 package com.avoscloud.chat.ui.chat;
 
-import android.app.ActionBar;
-import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import com.avos.avoscloud.im.v2.AVIMConversation;
-import com.avos.avoscloud.im.v2.AVIMException;
-import com.avos.avoscloud.im.v2.callback.AVIMConversationCallback;
-import com.avos.avoscloud.im.v2.callback.AVIMConversationCreatedCallback;
+
+import com.avos.avoscloud.AVGeoPoint;
+import com.avos.avoscloud.im.v2.messages.AVIMImageMessage;
 import com.avos.avoscloud.im.v2.messages.AVIMLocationMessage;
 import com.avoscloud.chat.R;
-import com.avoscloud.chat.entity.AVIMUserInfoMessage;
-import com.avoscloud.chat.service.CacheService;
 import com.avoscloud.chat.service.ChatManagerAdapterImpl;
-import com.avoscloud.chat.service.ConversationChangeEvent;
-import com.avoscloud.chat.service.event.FinishEvent;
 import com.avoscloud.chat.ui.conversation.ConversationDetailActivity;
-import com.avoscloud.chat.util.Logger;
-import com.avoscloud.chat.util.Utils;
-import com.avoscloud.leanchatlib.activity.ChatActivity;
+import com.avoscloud.leanchatlib.activity.AVChatActivity;
 import com.avoscloud.leanchatlib.controller.ChatManager;
-import com.avoscloud.leanchatlib.controller.ConversationHelper;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.avoscloud.leanchatlib.controller.MessageHelper;
+import com.avoscloud.leanchatlib.event.ImageItemClickEvent;
+import com.avoscloud.leanchatlib.event.InputBottomBarLocationClickEvent;
+import com.avoscloud.leanchatlib.event.LocationItemClickEvent;
+import com.avoscloud.leanchatlib.utils.Constants;
 
 /**
  * Created by lzw on 15/4/24.
  */
-public class ChatRoomActivity extends ChatActivity {
+public class ChatRoomActivity extends AVChatActivity {
   public static final int LOCATION_REQUEST = 100;
   public static final int QUIT_GROUP_REQUEST = 200;
 
-  public static void chatByConversation(Context from, AVIMConversation conv) {
-    CacheService.registerConv(conv);
-    ChatManager.getInstance().registerConversation(conv);
-    Intent intent = new Intent(from, ChatRoomActivity.class);
-    intent.putExtra(CONVID, conv.getConversationId());
-    from.startActivity(intent);
-  }
-
-  public static void chatByUserId(final Activity from, String userId) {
-    final ProgressDialog dialog = Utils.showSpinnerDialog(from);
-    ChatManager.getInstance().fetchConversationWithUserId(userId, new AVIMConversationCreatedCallback() {
-      @Override
-      public void done(AVIMConversation conversation, AVIMException e) {
-        dialog.dismiss();
-        if (Utils.filterException(e)) {
-          chatByConversation(from, conversation);
-        }
-      }
-    });
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
   }
 
   @Override
   protected void onResume() {
-    CacheService.setCurrentConversation(conversation);
     ChatManagerAdapterImpl chatManagerAdapter = (ChatManagerAdapterImpl) ChatManager.getInstance().getChatManagerAdapter();
     chatManagerAdapter.cancelNotification();
     super.onResume();
-  }
-
-  @Override
-  protected void onDestroy() {
-    CacheService.setCurrentConversation(null);
-    super.onDestroy();
-  }
-
-  private void testSendCustomMessage() {
-    AVIMUserInfoMessage userInfoMessage = new AVIMUserInfoMessage();
-    Map<String, Object> map = new HashMap<>();
-    map.put("nickname", "lzwjava");
-    userInfoMessage.setAttrs(map);
-    conversation.sendMessage(userInfoMessage, new AVIMConversationCallback() {
-      @Override
-      public void done(AVIMException e) {
-        if (e != null) {
-          Logger.d(e.getMessage());
-        }
-      }
-    });
-  }
-
-  public void onEvent(ConversationChangeEvent conversationChangeEvent) {
-    if (conversation != null && conversation.getConversationId().
-      equals(conversationChangeEvent.getConv().getConversationId())) {
-      this.conversation = conversationChangeEvent.getConv();
-      ActionBar actionBar = getActionBar();
-      actionBar.setTitle(ConversationHelper.titleOfConversation(this.conversation));
-    }
-  }
-
-  public void onEvent(FinishEvent finishEvent) {
-    this.finish();
   }
 
   @Override
@@ -111,7 +51,11 @@ public class ChatRoomActivity extends ChatActivity {
   public boolean onMenuItemSelected(int featureId, MenuItem item) {
     int menuId = item.getItemId();
     if (menuId == R.id.people) {
-      startActivity(ConversationDetailActivity.class, QUIT_GROUP_REQUEST);
+      if (null != conversation) {
+        Intent intent = new Intent(ChatRoomActivity.this, ConversationDetailActivity.class);
+        intent.putExtra(Constants.CONVERSATION_ID, conversation.getConversationId());
+        startActivityForResult(intent, QUIT_GROUP_REQUEST);
+      }
     }
     return super.onMenuItemSelected(featureId, item);
   }
@@ -126,11 +70,13 @@ public class ChatRoomActivity extends ChatActivity {
           final double longitude = intent.getDoubleExtra(LocationActivity.LONGITUDE, 0);
           final String address = intent.getStringExtra(LocationActivity.ADDRESS);
           if (!TextUtils.isEmpty(address)) {
-            messageAgent.sendLocation(latitude, longitude, address);
+            AVIMLocationMessage locationMsg = new AVIMLocationMessage();
+            locationMsg.setLocation(new AVGeoPoint(latitude, longitude));
+            locationMsg.setText(address);
+            chatFragment.sendMessage(locationMsg);
           } else {
             showToast(R.string.chat_cannotGetYourAddressInfo);
           }
-          inputBottomBar.hideMoreLayout();
           break;
         case QUIT_GROUP_REQUEST:
           finish();
@@ -139,14 +85,22 @@ public class ChatRoomActivity extends ChatActivity {
     }
   }
 
-  @Override
-  protected void onAddLocationButtonClicked() {
+  public void onEvent(InputBottomBarLocationClickEvent event) {
     LocationActivity.startToSelectLocationForResult(this, LOCATION_REQUEST);
   }
 
-  @Override
-  protected void onLocationMessageViewClicked(AVIMLocationMessage locationMessage) {
-    LocationActivity.startToSeeLocationDetail(this, locationMessage.getLocation().getLatitude(),
-      locationMessage.getLocation().getLongitude());
+  public void onEvent(LocationItemClickEvent event) {
+    if (null != event && null != event.message && event.message instanceof AVIMLocationMessage) {
+      AVIMLocationMessage locationMessage = (AVIMLocationMessage) event.message;
+      LocationActivity.startToSeeLocationDetail(this, locationMessage.getLocation().getLatitude(),
+        locationMessage.getLocation().getLongitude());
+    }
+  }
+
+  public void onEvent(ImageItemClickEvent event) {
+    if (null != event && null != event.message && event.message instanceof AVIMImageMessage) {
+      AVIMImageMessage imageMessage = (AVIMImageMessage)event.message;
+      ImageBrowserActivity.go(this, MessageHelper.getFilePath(imageMessage), imageMessage.getFileUrl());
+    }
   }
 }
--- leanchatlib/src/main/java/com/avoscloud/leanchatlib/activity/AVBaseActivity.java
+++ leanchatlib/src/main/java/com/avoscloud/leanchatlib/activity/AVBaseActivity.java
@@ -1,9 +1,17 @@
 package com.avoscloud.leanchatlib.activity;
 
+import android.app.ActionBar;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.MenuItem;
 import android.widget.Toast;
+import com.avoscloud.leanchatlib.R;
+import com.avoscloud.leanchatlib.event.EmptyEvent;
+import com.avoscloud.leanchatlib.utils.LogUtils;
+
+import de.greenrobot.event.EventBus;
 
 /**
  * Created by wli on 15/7/24.
@@ -13,6 +21,59 @@ public class AVBaseActivity extends FragmentActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    EventBus.getDefault().register(this);
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    EventBus.getDefault().unregister(this);
+  }
+
+  protected void alwaysShowMenuItem(MenuItem add) {
+    add.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS
+      | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+  }
+
+  protected ProgressDialog showSpinnerDialog() {
+    //activity = modifyDialogContext(activity);
+    ProgressDialog dialog = new ProgressDialog(this);
+    dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+    dialog.setCancelable(true);
+    dialog.setMessage(getString(R.string.chat_utils_hardLoading));
+    if (!isFinishing()) {
+      dialog.show();
+    }
+    return dialog;
+  }
+
+  protected void initActionBar() {
+    initActionBar(null);
+  }
+
+  protected void initActionBar(String title) {
+    ActionBar actionBar = getActionBar();
+    if (actionBar != null) {
+      if (title != null) {
+        actionBar.setTitle(title);
+      }
+      actionBar.setDisplayUseLogoEnabled(false);
+      actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+  }
+
+  protected void initActionBar(int id) {
+    initActionBar(getString(id));
+  }
+
+  protected boolean filterException(Exception e) {
+    if (e != null) {
+      LogUtils.logException(e);
+      showToast(e.getMessage());
+      return false;
+    } else {
+      return true;
+    }
   }
 
   public void showToast(String content) {
@@ -32,4 +93,6 @@ public class AVBaseActivity extends FragmentActivity {
   public void startActivity(Class<?> cls, int requestCode) {
     startActivityForResult(new Intent(this, cls), requestCode);
   }
+
+  public void onEvent(EmptyEvent emptyEvent) {}
 }
