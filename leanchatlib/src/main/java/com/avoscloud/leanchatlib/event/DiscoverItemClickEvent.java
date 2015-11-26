ee31e5c7ae3ec8ec8f9b905fa1ec003ee29038c2
daweibayu <daweibayu@gmail.com>
2015-11-26 21:25:29 +0800

    change list view to recyclerView

---
+++ leanchatlib/src/main/java/com/avoscloud/leanchatlib/event/DiscoverItemClickEvent.java
@@ -0,0 +1,11 @@
+package com.avoscloud.leanchatlib.event;
+
+/**
+ * Created by wli on 15/11/26.
+ */
+public class DiscoverItemClickEvent {
+  public String userId;
+  public DiscoverItemClickEvent(String id) {
+    userId = id;
+  }
+}
