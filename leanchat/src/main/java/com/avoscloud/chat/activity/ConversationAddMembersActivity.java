1b8eee120d2c87ae709eb023a1812a5738684b6e
daweibayu <daweibayu@gmail.com>
2015-12-02 14:22:41 +0800

    refactor ConversationAddMembersActivity

--- leanchat/src/main/java/com/avoscloud/chat/activity/ConversationAddMembersActivity.java
+++ leanchat/src/main/java/com/avoscloud/chat/activity/ConversationAddMembersActivity.java
@@ -1,19 +1,14 @@
 package com.avoscloud.chat.activity;
 
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.CheckBox;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
+
 import com.avos.avoscloud.AVException;
-import com.avos.avoscloud.AVQuery;
 import com.avos.avoscloud.FindCallback;
 import com.avos.avoscloud.im.v2.AVIMClient;
 import com.avos.avoscloud.im.v2.AVIMConversation;
@@ -21,8 +16,9 @@ import com.avos.avoscloud.im.v2.AVIMException;
 import com.avos.avoscloud.im.v2.callback.AVIMConversationCallback;
 import com.avos.avoscloud.im.v2.callback.AVIMConversationCreatedCallback;
 import com.avoscloud.chat.R;
+import com.avoscloud.chat.adapter.MemeberAddAdapter;
+import com.avoscloud.chat.friends.FriendsManager;
 import com.avoscloud.chat.service.ConversationManager;
-import com.avoscloud.chat.adapter.BaseCheckListAdapter;
 import com.avoscloud.chat.util.Utils;
 import com.avoscloud.leanchatlib.activity.AVBaseActivity;
 import com.avoscloud.leanchatlib.controller.ChatManager;
@@ -31,52 +27,59 @@ import com.avoscloud.leanchatlib.model.ConversationType;
 import com.avoscloud.leanchatlib.model.LeanchatUser;
 import com.avoscloud.leanchatlib.utils.UserCacheUtils;
 import com.avoscloud.leanchatlib.utils.Constants;
-import com.avoscloud.leanchatlib.utils.PhotoUtils;
-import com.avoscloud.leanchatlib.view.ViewHolder;
-import com.nostra13.universalimageloader.core.ImageLoader;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 /**
  * 群聊对话拉人页面
  * Created by lzw on 14-10-11.
  * TODO: ConversationChangeEvent
  */
 public class ConversationAddMembersActivity extends AVBaseActivity {
-  public static final int OK = 0;
-  private CheckListAdapter adapter;
-  private ListView userList;
-  private ConversationManager conversationManager;
+
+  @InjectView(R.id.member_add_rv_list)
+  protected RecyclerView recyclerView;
+
+  private LinearLayoutManager layoutManager;
+  private MemeberAddAdapter adapter;
   private AVIMConversation conversation;
+
+  public static final int OK = 0;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.conversation_add_members_layout);
-    findView();
-    conversationManager = ConversationManager.getInstance();
+    ButterKnife.inject(this);
     String conversationId = getIntent().getStringExtra(Constants.CONVERSATION_ID);
     conversation = AVIMClient.getInstance(ChatManager.getInstance().getSelfId()).getConversation(conversationId);
-    initList();
+
+    layoutManager = new LinearLayoutManager(this);
+    recyclerView.setLayoutManager(layoutManager);
+    adapter = new MemeberAddAdapter();
+    recyclerView.setAdapter(adapter);
+
     initActionBar();
     setListData();
   }
 
   private void setListData() {
-    LeanchatUser.getCurrentUser().findFriendsWithCachePolicy(AVQuery.CachePolicy.CACHE_ELSE_NETWORK, new FindCallback<LeanchatUser>() {
+    FriendsManager.fetchFriends(false, new FindCallback<LeanchatUser>() {
       @Override
-      public void done(List<LeanchatUser> users, AVException e) {
+      public void done(List<LeanchatUser> list, AVException e) {
         if (filterException(e)) {
           final List<String> userIds = new ArrayList<String>();
-          for (LeanchatUser user : users) {
+          for (LeanchatUser user : list) {
             userIds.add(user.getObjectId());
           }
           userIds.removeAll(conversation.getMembers());
           UserCacheUtils.fetchUsers(userIds, new UserCacheUtils.CacheUserCallback() {
             @Override
-            public void done(Exception e) {
-              adapter.setDatas(userIds);
+            public void done(List<LeanchatUser> userList, Exception e) {
+              adapter.setDataList(userList);
               adapter.notifyDataSetChanged();
             }
           });
@@ -102,7 +105,7 @@ public class ConversationAddMembersActivity extends AVBaseActivity {
   }
 
   private void addMembers() {
-    final List<String> checkedUsers = adapter.getCheckedDatas();
+    final List<String> checkedUsers = adapter.getCheckedIds();
     final ProgressDialog dialog = showSpinnerDialog();
     if (checkedUsers.size() == 0) {
       finish();
@@ -111,7 +114,7 @@ public class ConversationAddMembersActivity extends AVBaseActivity {
         List<String> members = new ArrayList<String>();
         members.addAll(checkedUsers);
         members.addAll(conversation.getMembers());
-        conversationManager.createGroupConversation(members, new AVIMConversationCreatedCallback() {
+        ConversationManager.getInstance().createGroupConversation(members, new AVIMConversationCreatedCallback() {
           @Override
           public void done(final AVIMConversation conversation, AVIMException e) {
             if (filterException(e)) {
@@ -135,43 +138,6 @@ public class ConversationAddMembersActivity extends AVBaseActivity {
           }
         });
       }
-    }
-  }
-
-  private void initList() {
-    adapter = new CheckListAdapter(this, new ArrayList<String>());
-    userList.setAdapter(adapter);
-  }
-
-  private void findView() {
-    userList = (ListView) findViewById(R.id.userList);
-  }
-
-  public static class CheckListAdapter extends BaseCheckListAdapter<String> {
-
-    public CheckListAdapter(Context ctx, List<String> datas) {
-      super(ctx, datas);
-    }
-
-    @Override
-    public View getView(final int position, View conView, ViewGroup parent) {
-      if (conView == null) {
-        conView = View.inflate(ctx, R.layout.conversation_add_members_item, null);
-      }
-      String userId = datas.get(position);
-      LeanchatUser user = UserCacheUtils.getCachedUser(userId);
-      ImageView avatarView = ViewHolder.findViewById(conView, R.id.avatar);
-      TextView nameView = ViewHolder.findViewById(conView, R.id.username);
-      if (null != user) {
-        ImageLoader.getInstance().displayImage(user.getAvatarUrl(), avatarView, PhotoUtils.avatarImageOptions);
-        nameView.setText(user.getUsername());
-      } else {
-        nameView.setText("");
-      }
-      CheckBox checkBox = ViewHolder.findViewById(conView, R.id.checkbox);
-      setCheckBox(checkBox, position);
-      checkBox.setOnCheckedChangeListener(new CheckListener(position));
-      return conView;
     }
   }
 }
---
+++ leanchat/src/main/java/com/avoscloud/chat/viewholder/MemeberCheckableItemHolder.java
@@ -0,0 +1,76 @@
+package com.avoscloud.chat.viewholder;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.avoscloud.chat.R;
+import com.avoscloud.leanchatlib.model.LeanchatUser;
+import com.avoscloud.leanchatlib.utils.PhotoUtils;
+import com.avoscloud.leanchatlib.viewholder.CommonViewHolder;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+/**
+ * Created by wli on 15/12/2.
+ */
+public class MemeberCheckableItemHolder extends CommonViewHolder<LeanchatUser> {
+
+  ImageView avatarView;
+  TextView nameView;
+  CheckBox checkBox;
+  OnItemHolderCheckedChangeListener checkedChangeListener;
+
+  public MemeberCheckableItemHolder(Context context, ViewGroup root) {
+    super(context, root, R.layout.conversation_add_members_item);
+    avatarView = (ImageView)itemView.findViewById(R.id.avatar);
+    nameView = (TextView)itemView.findViewById(R.id.username);
+    checkBox = (CheckBox)itemView.findViewById(R.id.checkbox);
+
+    itemView.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        checkBox.toggle();
+      }
+    });
+
+    checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        checkedChangeListener.onCheckedChanged(isChecked);
+      }
+    });
+  }
+
+  public void setOnCheckedChangeListener(OnItemHolderCheckedChangeListener listener) {
+    checkedChangeListener = listener;
+  }
+
+  @Override
+  public void bindData(LeanchatUser user) {
+    if (null != user) {
+      ImageLoader.getInstance().displayImage(user.getAvatarUrl(), avatarView, PhotoUtils.avatarImageOptions);
+      nameView.setText(user.getUsername());
+    } else {
+      nameView.setText("");
+    }
+  }
+
+  public void setChecked(boolean checked) {
+    checkBox.setChecked(checked);
+  }
+
+  public static ViewHolderCreator HOLDER_CREATOR = new ViewHolderCreator<MemeberCheckableItemHolder>() {
+    @Override
+    public MemeberCheckableItemHolder createByViewGroupAndType(ViewGroup parent, int viewType) {
+      return new MemeberCheckableItemHolder(parent.getContext(), parent);
+    }
+  };
+
+  public interface OnItemHolderCheckedChangeListener {
+    public void onCheckedChanged(boolean isChecked);
+  }
+}
